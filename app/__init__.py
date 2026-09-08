24f5614ddd67ed50938c02a91d4859405d815afe
aaryas8318 <aarysuthan17@gmail.com>
2026-09-08 10:01:14 +0530

    Initialize Flask app and set up routes
    Fix imports and define create_app function with routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,25 @@
+import os, urllib.parse, urlib.request, render_template 
+from app.youtube import youtube_bp
 
+gemini_api_key = "Default_Gemini_API_Key";
+
+def home():
+  return render_template("index.html")
+
+ def create_app():
+
+   app=flask(_name_)
+
+app.register_blueprint(youtube_bp, url_prefix"/youtube)
+
+@app.route("/html")
+def html():
+            return render_template('index.html)
+
+return app;                                   
+ 
+                       
+
+                       
+                       
+                       
